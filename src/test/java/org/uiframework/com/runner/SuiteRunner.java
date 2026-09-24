ae98d3ef888627464fe2b6c2d34644d69603d336
Daniel <danielrune13@hotmail.com>
2026-09-24 06:44:16 +0100

    Add back serenity cucumber plugin to SuiteRunner

--- src/test/java/org/uiframework/com/runner/SuiteRunner.java
+++ src/test/java/org/uiframework/com/runner/SuiteRunner.java
@@ -11,6 +11,6 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features/")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm,pretty,json:target/cucumber-reports/report.json,html:target/html-reports/report.html")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "net.serenitybdd.cucumber.core.plugin.SerenityReporterParallel,io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm,pretty,json:target/cucumber-reports/report.json,html:target/html-reports/report.html")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.uiframework.com")
 public class SuiteRunner {}
